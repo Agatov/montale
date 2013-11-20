b61f409a78d9f135044ee758ef196f62b3d45671
agatov <agatovs@gmail.com>
2013-11-20 14:33:09 +0400

    initial commit

---
+++ application.rb
@@ -0,0 +1,55 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'haml'
+require 'sass'
+require 'httparty'
+require 'json'
+
+class Application < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  set :sass, { :load_paths => [ "#{Application.root}/assets/stylesheets" ] }
+  set :protection, :except => :frame_options
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/css', from: 'assets/stylesheets'
+    serve '/images', from: 'assets/images'
+    serve '/js', from: 'assets/javascripts'
+    serve '/fonts', from: 'assets/fonts'
+
+    css :application, '/css/application.css', %w(/css/reset.css /css/index.css /css/current.css /css/modal.css)
+    js :application, '/js/application.js', %w( /js/jquery-1.9.1.js /js/order.js)
+
+    css_compression :sass
+    js_compression :jsmin
+  }
+
+  get '/' do
+    haml :index
+  end
+
+  post '/orders.json' do
+
+    phones = %w(79037928959)
+
+    message = "#{params[:order][:username]}. #{params[:order][:phone]}. #{params[:order][:email]}"
+
+    phones.each do |phone|
+      HTTParty.get(
+          'http://api.sms24x7.ru',
+          query: {
+              method: 'push_msg',
+              email: 'agatovs@gmail.com',
+              password: 'avv6rqE',
+              phone: phone.to_s,
+              text: message,
+              sender_name: '+79689124622'
+          }
+      )
+    end
+
+    content_type :json
+    {status: :success}.to_json
+  end
+end
